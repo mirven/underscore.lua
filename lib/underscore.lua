233053427ea57274d19b9dbba9d4d1270f5d92f3
Marcus Irven <marcus@marcusirven.com>
2009-12-04 11:14:23 -0600

    towards better documentation

--- lib/underscore.lua
+++ lib/underscore.lua
@@ -1,5 +1,29 @@
+-- Copyright (c) 2009 Marcus Irven
+--  
+-- Permission is hereby granted, free of charge, to any person
+-- obtaining a copy of this software and associated documentation
+-- files (the "Software"), to deal in the Software without
+-- restriction, including without limitation the rights to use,
+-- copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the
+-- Software is furnished to do so, subject to the following
+-- conditions:
+--  
+-- The above copyright notice and this permission notice shall be
+-- included in all copies or substantial portions of the Software.
+--  
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+-- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+-- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+-- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+-- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+-- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE.
+
 --- Underscore is a set of utility functions for dealing with 
 -- iterators, arrays, tables, and functions.
+
 local previous_underscore = _
 
 local Underscore = { funcs = {} }
@@ -33,9 +57,6 @@ function Underscore.identity(value)
 	return value
 end
 
---- Restores the _ variable and returns the _ object so that 
--- it can be assigned to another variable.
--- @name _:no_conflict
 function Underscore:no_conflict()
   _ = previous_underscore
   return self
@@ -43,27 +64,17 @@ end
 
 -- chaining
 
---- Starts chaining.
--- @name _:chain
 function Underscore:chain()
 	self.chained = true
 	return self
 end
 
---- Returns the value of a chained object.
--- @name _:value
 function Underscore:value()
 	return self._val
 end
 
 -- iter
 
---- Calls func on each item in list.
--- @name _.each
--- @param list
--- @param func
--- @return list
--- @usage _.each({1,2,3}, print)
 function Underscore.funcs.each(list, func)
 	for i in Underscore.iter(list) do
 		func(i)
@@ -71,12 +82,6 @@ function Underscore.funcs.each(list, func)
 	return list
 end
 
---- Produces a new array by mapping each value in iter
--- through a transformation function.
--- @name _.map
--- @param list
--- @param func
--- @usage _.map({1,2,3}, function(i) return i*2 end)
 function Underscore.funcs.map(list, func)
 	local mapped = {}
 	for i in Underscore.iter(list) do
@@ -85,12 +90,6 @@ function Underscore.funcs.map(list, func)
 	return mapped
 end
 
---- Reduces a list of items down to a singular value.
--- @name _.reduce
--- @param list items to perform the reduction on
--- @param memo initial state of reduction and each sucessive state should be returned by func
--- @param func reduction function which takes two parameters, the current state and value
--- @usage _.reduce({1,2,3}, 0, function(memo, i) return memo+i end)
 function Underscore.funcs.reduce(list, memo, func)	
 	for i in Underscore.iter(list) do
 		memo = func(memo, i)
@@ -98,12 +97,6 @@ function Underscore.funcs.reduce(list, memo, func)
 	return memo
 end
 
---- Looks through a list returning the first element that matches 
--- a truth function. The function returns as soon as it finds an
--- acceptable element and doesn't traverse the entire list.
--- @name _.detect
--- @param list
--- @param func
 function Underscore.funcs.detect(list, func)
 	for i in Underscore.iter(list) do
 		if func(i) then return i end
@@ -111,11 +104,6 @@ function Underscore.funcs.detect(list, func)
 	return nil	
 end
 
---- Looks through a list and returns a new array containing all
--- items that match a truth function.
--- @name _.select
--- @param list
--- @param func
 function Underscore.funcs.select(list, func)
 	local selected = {}
 	for i in Underscore.iter(list) do
@@ -124,11 +112,6 @@ function Underscore.funcs.select(list, func)
 	return selected
 end
 
---- Looks through alist and returns a new array containing all
--- items that don't match the truth function.
--- @name _.reject
--- @param list
--- @param func
 function Underscore.funcs.reject(list, func)
 	local selected = {}
 	for i in Underscore.iter(list) do
@@ -137,11 +120,6 @@ function Underscore.funcs.reject(list, func)
 	return selected
 end
 
---- Returns true if func(item) returns true for all item in items.
--- @name _.all
--- @param list items
--- @param func (optional) 
--- @usage _.all({2,4,8}, function(i) return i%2 end)
 function Underscore.funcs.all(list, func)
 	func = func or Underscore.identity
 	
@@ -152,11 +130,6 @@ function Underscore.funcs.all(list, func)
 	return true
 end
 
---- Returns true if func(item) returns true for all item in items.
--- @name _.any
--- @param list items
--- @param func (optional) 
--- @usage _.any({2,4,8}, function(i) return i%2 end)
 function Underscore.funcs.any(list, func)
 	func = func or Underscore.identity
 
@@ -167,10 +140,6 @@ function Underscore.funcs.any(list, func)
 	return false
 end
 
---- Returns true if the list include's value.
--- @name _.include
--- @param list
--- @param value
 function Underscore.funcs.include(list, value)
 	for i in Underscore.iter(list) do
 		if i == value then return true end
@@ -178,48 +147,16 @@ function Underscore.funcs.include(list, value)
 	return false
 end
 
---- Calls a function with specified name on each item using the colon operator.
--- @name _.invoke
--- @param list
--- @param function_name
--- @param ... (optional) arguments to be passed into the function
--- @return the original list
--- @usage Person = {}; Person.__index = Person
---		function Person:new(name)
---			return setmetatable({ name=name }, self)
---		end
---		function Person:print()
---			print(self.name)
---		end
---		_.invoke({ Person:new("Tom"), Person:new("Dick"), Person:new("Harry") }, "print")
--- => Calls person:print() on each Person
 function Underscore.funcs.invoke(list, function_name, ...)
 	local args = {...}
 	Underscore.funcs.each(list, function(i) i[function_name](i, unpack(args)) end)
 	return list
 end
 
---- An convenient version of the common use-case of map: extracting a list of properties.
--- @name _.pluck
--- @param list
--- @param property_name
--- @return new array containing the property value corresponding to the propery name on each item
--- @usage _.pluck({ {id=1}, {id=2}, {id=3} }, 'id')
--- => { 1, 2, 3 }
 function Underscore.funcs.pluck(list, propertyName)
 	return Underscore.funcs.map(list, function(i) return i[propertyName] end)
 end
 
---- Returns the item with the smallest value. If a func i spassed it will be used on each value 
--- to generate the criterion by which the value is ranked.
--- @name _.min
--- @param list
--- @param func
--- @return the minimum value
--- @usage _.min({ 5, 2, 15})
--- => 2
--- _.min({ {age=5}, {age=2}, {age=15} }, function(p) return p.age end)
--- => {age=2}
 function Underscore.funcs.min(list, func)
 	func = func or Underscore.identity
 	
@@ -238,16 +175,6 @@ function Underscore.funcs.min(list, func)
 	end).item
 end
 
---- Returns the item with the largest value. If a func i spassed it will be used on each value 
--- to generate the criterion by which the value is ranked.
--- @name _.max
--- @param list
--- @param func
--- @return the maximum value
--- @usage _.max({ 5, 2, 15})
--- => 15
--- _.max({ {age=5}, {age=2}, {age=15} }, function(p) return p.age end)
--- => {age=15}
 function Underscore.funcs.max(list, func)
 	func = func or Underscore.identity
 	
@@ -266,13 +193,6 @@ function Underscore.funcs.max(list, func)
 	end).item
 end
 
---- Converts the list to an array. This is useful for creating an
--- array from a generator.
--- @name _.to_array
--- @param list
--- @return a new array
--- @usage _.to_array(string.gmatch("dog cat goat", "%S+"))
--- => { "dog", "cat", "goat" }
 function Underscore.funcs.to_array(list)
 	local array = {}
 	for i in Underscore.iter(list) do
@@ -281,13 +201,6 @@ function Underscore.funcs.to_array(list)
 	return array
 end
 
---- Iterates over all the items and returns a new array with the items
--- in reverse order.
--- @name _.reverse
--- @param list
--- @return a new array
--- @usage _.reverse({ 1, 2, 3})
--- => { 3, 2, 1 }
 function Underscore.funcs.reverse(list)
 	local reversed = {}
 	for i in Underscore.iter(list) do
@@ -296,14 +209,6 @@ function Underscore.funcs.reverse(list)
 	return reversed
 end
 
---- Sorts all items using a comparison function if provided. Uses table.sort but
--- creates a new array first.
--- @name _.sort
--- @param list
--- @param comparison_func (Optional)
--- @return a new array
--- @usage _.sort({ 10, 2, 5})
--- => { 2, 5, 10 }
 function Underscore.funcs.sort(iter, comparison_func)
 	local array = iter
 	if type(iter) == "function" then
@@ -315,16 +220,6 @@ end
 
 -- arrays
 
---- Returns the first element of an array. Optionally it will return an array of the 
--- first n items.
--- @name _.first
--- @param array
--- @param n (Optional)
--- @return the first item or an array of the first n items of n is specified
--- @usage _.first({ 10, 2, 5})
--- => 10
--- _.first({ 10, 2, 5}, 2)
--- => { 10, 2 }
 function Underscore.funcs.first(array, n)
 	if n == nil then
 		return array[1]
@@ -338,15 +233,6 @@ function Underscore.funcs.first(array, n)
 	end
 end
 
---- Returns an array of all items except the first. Optionally it will start at index.
--- @name _.rest
--- @param array
--- @param index (Optional)
--- @return an array with the remaining items
--- @usage _.rest({ 1, 2, 3 })
--- => { 2,3 }
--- _.rest({ 1, 2, 3 }, 2)
--- => { 3 }
 function Underscore.funcs.rest(array, index)
 	index = index or 2
 	local rest = {}
@@ -356,14 +242,6 @@ function Underscore.funcs.rest(array, index)
 	return rest
 end
 
---- Returns a section of an array starting with start_index of length items length.
--- @name _.slice
--- @param array
--- @param start_index
--- @param length
--- @return an array
--- @usage _.slice({ 1, 2, 3, 4, 5 }, 2, 3)
--- => { 2, 3, 4 }
 function Underscore.funcs.slice(array, start_index, length)
 	local sliced_array = {}
 	
@@ -375,12 +253,6 @@ function Underscore.funcs.slice(array, start_index, length)
 	return sliced_array
 end
 
---- Flattens a nested array (the nesting can be to any depth). 
--- @name _.flatten
--- @param array
--- @return the flattened array
--- @usage _.flatten({1, {2}, {3, {{{4}}}}})
--- => { 1, 2, 3, 4 }
 function Underscore.funcs.flatten(array)
 	local all = {}
 	
@@ -395,37 +267,17 @@ function Underscore.funcs.flatten(array)
 	return all
 end
 
---- Inserts and item on the back of of an array
--- @name _.push
--- @return the array
--- @usage _.push({1,2,3}, 4)
--- => { 1,2,3,4 }
 function Underscore.funcs.push(array, item)
 	array[#array+1] = item
 	return array
 end
 
---- Returns a string with all items in the array concatenated together with an optional separator
--- @name _.join
--- @param array
--- @param separator (Optional)
--- @return 
--- @usage _.join({'c','a','t'})
--- => "cat"
--- _.join({'c','a','t'}, '/')
--- => "c/a/t"
 function Underscore.funcs.join(array, separator)
 	return table.concat(array, separator)
 end
 
 -- objects
 
---- Returns an array of all the property names in a table.
--- @name _.keys
--- @param obj
--- @return an array with the property names (Note: the order is not defined)
--- @usage _.keys { name = "John", age = 25 }
--- => { "name", "age" }
 function Underscore.funcs.keys(obj)
 	local keys = {}
 	for k,v in pairs(obj) do
@@ -434,12 +286,6 @@ function Underscore.funcs.keys(obj)
 	return keys
 end
 
---- Returns an array of all the property values in a table.
--- @name _.values
--- @param obj
--- @return an array with the property values (Note: the order is not defined)
--- @usage _.values { name = "John", age = 25 }
--- => { "John", 25 }
 function Underscore.funcs.values(obj)
 	local values = {}
 	for k,v in pairs(obj) do
@@ -448,13 +294,6 @@ function Underscore.funcs.values(obj)
 	return values
 end
 
---- Copy all of the properties in the source object over to the destination object. 
--- @name _.values
--- @param destination
--- @param source
--- @return the destination object
--- @usage _.extend({ name = 'moe' }, { age = 50 })
--- => { name = 'moe', age = 50 }
 function Underscore.funcs.extend(destination, source)
 	for k,v in pairs(source) do
 		destination[k] = v
@@ -462,27 +301,12 @@ function Underscore.funcs.extend(destination, source)
 	return destination
 end
 
---- Returns true if object contains no values. 
--- @name _.is_empty
--- @usage _.is_empty({})
--- => true
--- -.is_empty({ name = "moe" })
--- => false
 function Underscore.funcs.is_empty(obj)
 	return next(obj) == nil
 end
 
 -- functions
 
---- Returns the composition of a list of functions, where each function consumes the 
--- return value of the function that follows. In math terms, composing the functions 
--- f(), g(), and h() produces f(g(h())). 
--- @name _.compose
--- @usage greet = function(name) return "hi: "..name end
--- exclaim = function(statement) return statement.."!" end
--- welcome = _.compose(print, greet, exclaim)
--- welcome('moe')
--- => hi: moe!
 function Underscore.funcs.compose(...)
 	local function call_funcs(funcs, ...)
 		if #funcs > 1 then
@@ -498,27 +322,12 @@ function Underscore.funcs.compose(...)
 	end
 end
 
---- Wraps the first function inside of the wrapper function, 
--- passing it as the first argument. This allows the wrapper to 
--- execute code before and after the function runs, adjust the 
--- arguments, and execute it conditionally.
--- @name _.wrap
--- @usage local hello = function(name) return "hello: "..name end
--- hello = _.wrap(hello, function(func, ...)
---   return "before, "..func(...)..", after"
--- end)
--- hello('moe')
--- => before, hello: moe, after
 function Underscore.funcs.wrap(func, wrapper)
 	return function(...)
 		return wrapper(func, ...)
 	end
 end
 
---- Returns an array of all function names in this library.
--- @name _.functions
--- @usage _.functions()
--- => { 'each', 'map', 'reduce', ... }
 function Underscore.functions() 
 	return Underscore.keys(Underscore.funcs)
 end
