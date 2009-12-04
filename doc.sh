8bc7a5e688c97a6cd5d5105a36423b560941b717
Marcus Irven <marcus@marcusirven.com>
2009-12-04 15:49:11 -0600

    remove old luadoc script

--- doc.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# ~/.luarocks/bin/luadoc lib/* -d doc -doclet 'doclet.mirven'
-
-~/.luarocks/bin/luadoc lib/* -d doc -t template
-mkdir -p docs
-cp doc/files/lib/underscore.html docs/index.html
-cp doc/luadoc.css docs/main.css
-rm -rf doc
